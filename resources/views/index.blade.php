6d8f156f01362e9c90c0c80f6bf98a5282bea1d6
phasakorn09 <nudangtop@gmail.com>
2024-10-04 02:26:29 +0700

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bad Luck</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div id ="layout">
+        <header class="AppHeader Layout_top-part">
+            <div class="AppHeader_right">
+                <div>
+                    <button class="LoginButton">
+                        "Sign In"
+                    </button>
+                </div>
+            </div>
+        </header>
+        <section class="AppPage_section section--control">
+            <div class="section_tapes">
+                <div class="tapes_tape">
+                    <div class="tapes_actions">
+
+                    </div>
+                    <div class="ContainerTape_inner">
+                        <ul class="ContainerTape_list list--ready">
+                            <li class="ContainerTape_item"> <div class="item_name">
+                                <span class="item_name-featured">นาย สมเกียรติ สายทอง</span>
+                                <br> "บอล-01"
+                            </div>     </li>
+                            <li class="ContainerTape_item"> <div class="item_name">
+                                <span class="item_name-featured">นาย ชนินทร์ จำนงค์จิรานุวัฒน์</span>
+                                <br> "เต้ย-02"
+                            </div>        </li>
+                            <li class="ContainerTape_item"> <div class="item_name">
+                                <span class="item_name-featured">นาย กันตินันท์ จินตนา</span>
+                                <br> "คิม-03"
+                            </div>        </li>
+                            <li class="ContainerTape_item"> <div class="item_name">
+                                <span class="item_name-featured">นาย วงศธร ทุ่งกระโทก</span>
+                                <br> "โม่-06"
+                            </div>        </li>
+                            <li class="ContainerTape_item"> <div class="item_name">
+                                <span class="item_name-featured">นาย ภาณุวิชญ์ เถื่อนประดิษฐ์</span>
+                                <br> "ปลื้ม-09"
+                            </div>        </li>
+                            <li class="ContainerTape_item"> <div class="item_name">
+                                <span class="item_name-featured">นาย ศุภชัย กุลประสิทธิ์ชัย</span>
+                                <br> "แบงค์-10"
+                            </div>        </li>
+                            <li class="ContainerTape_item"> <div class="item_name">
+                                <span class="item_name-featured">นาย วิวัฒน์ ระกาศ</span>
+                                <br> "ต้น-11"
+                            </div>        </li>
+                            <li class="ContainerTape_item"> <div class="item_name">
+                                <span class="item_name-featured">นาย อมรเทพ ถึงทรัพย์</span>
+                                <br> "แจ็ค-12"
+                            </div>        </li>
+                            <li class="ContainerTape_item"> <div class="item_name">
+                                <span class="item_name-featured">นาย ไตรเพชร ทองทวี</span>
+                                <br> "จิน-15"
+                            </div>        </li>
+                            <li class="ContainerTape_item"> <div class="item_name">
+                                <span class="item_name-featured">นาย ภาสกร กิตติคุณาดุลย์</span>
+                                <br> "แดง-19"
+                            </div>        </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="ContainerControl ContainerControl--initialized">
+                
+            </div>
+        </section>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
